cf8fa58ab53ade3378cdd4ba6685943636ee5e2f
Nelson Martins <nelsonttmartins@gmail.com>
2024-04-04 09:42:22 +0100

    update

--- vcones.c
+++ vcones.c
@@ -4,12 +4,22 @@
 
 int main(int argc, char **argv){
     if(argc==2){
-        cone c[atoi(argv[1])-1];
-        printf("cones: %d\n", atoi(argv[1]));
-        for(int i=0;i<atoi(argv[1]);i++){
-            novo_cone(&c[i]);
-            printf("%d] raio: %f\t altura: %f\n", i+1, c[i].raio, c[i].altura)
+        int n = atoi(argv[1]);
+
+        cone *c = malloc(n * sizeof(cone));
+        if (c == NULL) {
+            printf("Memory allocation failed.\n");
+            return 1;
         }
+
+        printf("cones: %d\n",  n);
+        for(int i=0;i<n;i++){
+            novo_cone(&c[i]);
+        }
+        for(int i=0;i<n;i++){
+            printf("%d] raio: %f\t altura: %f\n", i+1, c[i].raio, c[i].altura);
+        }
+        free(c);
     }
     return 0;
 }

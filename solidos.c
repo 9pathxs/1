0d829bb7d5d1cbe406220b7443f1a8b95c92ba27
Nelson Martins <nelsonttmartins@gmail.com>
2024-04-04 09:18:50 +0100

    update

--- solidos.c
+++ solidos.c
@@ -1,9 +1,13 @@
 #include "solidos.h"
-#include <math.h>
+#include <stdio.h>
 
 float volume_cone (cone c){
     return ((1.0/3.0)*c.altura)*(pi*c.raio*c.raio);
 }
 float volume_cilin (cilindro c){
     return (pi*c.raio*c.raio*c.altura);
+}
+void novo_cone(cone *c){
+    scanf("%f", &((*c).altura));
+    scanf("%f", &((*c).raio));
 }
